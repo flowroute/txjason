6029f743fbe75c8e3c849cca11d40aa0ac760c62
Matthew Williams <mgwilliams@gmail.com>
2013-05-23 11:10:04 -0700

    remove unneeded dependency

--- setup.py
+++ setup.py
@@ -4,11 +4,9 @@ from setuptools import setup
 
 setup(name='txjason',
       version='0.1.0',
-      description='jsonrpc 2.0 module',
+      description='Twisted jsonrpc 2.0 module',
       author='Flowroute LLC',
       author_email='matthew@flowroute.com',
       packages=['txjason'],
-      install_requires=[
-          'jsonrpcbase',
-      ],
+      install_requires=[],
 )
