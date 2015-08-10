ece3807113d14453af1bad38288c1b6a9aa9d407
Jordan Levy <jordan@flowroute.com>
2015-08-10 11:36:11 -0700

    Remove buildout.

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-#!/usr/bin/env python
-
 from setuptools import setup
 
 setup(name='txjason',
       version='0.1.0',
       description='Twisted jsonrpc 2.0 module',
-      author='Flowroute LLC',
-      author_email='matthew@flowroute.com',
+      author='Flowroute Inc.',
+      author_email='development@flowroute.com',
       url='https://github.com/flowroute/txjason',
       packages=['txjason'],
-      install_requires=[],
+      install_requires=[
+          'Twisted',
+      ],
 )
