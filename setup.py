6ca738b15fc470b6cc3f35cc167f6c325d25c7b7
Jordan Levy <jordan@flowroute.com>
2015-08-10 11:38:08 -0700

    Fix linting errors.

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup
 
-setup(name='txjason',
-      version='0.1.0',
-      description='Twisted jsonrpc 2.0 module',
-      author='Flowroute Inc.',
-      author_email='development@flowroute.com',
-      url='https://github.com/flowroute/txjason',
-      packages=['txjason'],
-      install_requires=[
-          'Twisted',
-      ],
+setup(
+    name='txjason',
+    version='0.1.0',
+    description='Twisted jsonrpc 2.0 module',
+    author='Flowroute Inc.',
+    author_email='development@flowroute.com',
+    url='https://github.com/flowroute/txjason',
+    packages=['txjason'],
+    install_requires=[
+        'Twisted',
+    ],
 )
